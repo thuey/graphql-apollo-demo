3d81196b488e49be415fc5b6f893e69003c6078e
EC2 Default User <ec2-user@ip-172-31-20-214.ec2.internal>
2019-02-27 06:31:47 +0000

    simple apollo ui for graphql demo backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import './App.css';
 
+const GET_ARTISTS = gql`
+  query artistList {
+    artists {
+      id
+      name
+    }
+  }
+`;
+
+const GET_ARTIST_WITH_SONGS = gql`
+  query artistWithSongs($artistId: ID!) {
+    artist(id: $artistId) {
+      songs {
+        id
+        name
+      }
+    }
+  }
+`;
+
 class App extends Component {
+  
+  state = {};
+  
+  showSongsForArtist = artistId => {
+    this.setState({ artistId });
+  };
+  
   render() {
+    const { artistId } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Query query={GET_ARTISTS}>
+          {({ data, loading }) => {
+            if (loading) return 'Loading...';
+            return (
+              <ul>
+                {data.artists.map(({id, name}) =>
+                  <li onClick={() => this.showSongsForArtist(id)} key={id}>{name}</li>)
+                }
+              </ul>
+            );
+          }}
+        </Query>
+        <hr/>
+        {artistId && (
+          <Query query={GET_ARTIST_WITH_SONGS} variables={{ artistId }}>
+            {({ data, loading }) => {
+              if (loading) return 'Loading...';
+              return (
+                <Fragment>
+                  Songs:
+                  <ul>
+                    {data.artist.songs.map(({id, name}) =>
+                      <li key={id}>{name}</li>)
+                    }
+                  </ul>
+                </Fragment>
+              );
+            }}
+          </Query>
+        )}
+      </Fragment>
     );
   }
 }
